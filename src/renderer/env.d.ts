6ae1866805cb55c50a198de306cdb3a8c185b92b
elkon <elkon@qq.com>
2023-12-25 10:45:08 +0800

    cmd:add commit push

--- src/renderer/env.d.ts
+++ src/renderer/env.d.ts
@@ -6,3 +6,13 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>;
   export default component
 }
+
+// see: https://cn.electron-vite.org/guide/env-and-mode
+interface ImportMetaEnv {
+  readonly MAIN_VITE_SOME_KEY: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
