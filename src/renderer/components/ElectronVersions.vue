45f4fe6674d165dacd3ea96930718ec6fe04fc34
elkon <elkon@qq.com>
2023-12-31 13:26:22 +0800

    cmd:add commit push

--- src/renderer/components/ElectronVersions.vue
+++ src/renderer/components/ElectronVersions.vue
@@ -5,21 +5,21 @@ const electronVersions = reactive({ ...window.electron.process.versions })
 </script>
 
 <template>
-  <ul class="flex">
-    <li class="p-12px">
+  <div class="flex">
+    <div class="p-12px">
       Electron v{{ electronVersions.electron }}
-    </li>
-    <li class="p-12px">
+    </div>
+    <div class="p-12px">
       Chromium v{{ electronVersions.chrome }}
-    </li>
-    <li class="p-12px">
+    </div>
+    <div class="p-12px">
       Node v{{ electronVersions.node }}
-    </li>
-    <li class="flex-auto p-12px">
+    </div>
+    <div class="flex-auto p-12px">
       V8 v{{ electronVersions.v8 }}
-    </li>
-    <li class="p-12px">
+    </div>
+    <div class="p-12px">
       <DarkToggle />
-    </li>
-  </ul>
+    </div>
+  </div>
 </template>
